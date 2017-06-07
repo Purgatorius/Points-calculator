0c784e63d05a99130bf492a98bede5de1332b721
Purgatorius <Bartlomiej.Bugala@gmail.com>
2017-06-07 11:16:49 +0200

    Update code_PointsCalculator.java

--- code_PointsCalculator.java
+++ code_PointsCalculator.java
@@ -8,12 +8,16 @@ public class Main {
     {
         System.out.println("This is the program that calculate points. We've got points for the style, effects and result-each one is a different weight point");
         Scanner scanner = new Scanner(System.in);
+        
         System.out.println("Style: ");
         double styl = scanner.nextDouble() * 0.2;
+        
         System.out.println("Effects: ");
         double effects = scanner.nextDouble() * 0.3;
+        
         System.out.println("Result: ");
         double result = scanner.nextDouble() * 0.5;
+        
         double suma = styl + effects + result;
         System.out.println("_____________________");
         System.out.println("Ok, the sum of the points is: ");
