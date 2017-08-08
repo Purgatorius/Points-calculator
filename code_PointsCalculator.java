43c3c96373880f79d7eaa447becd5a75fc5aa2ff
Purgatorius <Bartlomiej.Bugala@gmail.com>
2017-08-08 16:15:03 +0200

    Update code_PointsCalculator.java

--- code_PointsCalculator.java
+++ code_PointsCalculator.java
@@ -1,26 +1,37 @@
 import java.util.Scanner;
 
 /**
- * Created by B4RT on 2017-06-07.
+ * Created by B4RT on 2017-08-08.
  */
-public class Main {
-    public static void main(String[] args)
+public class Zadanie3 {
+    public static void main (String[] args)
     {
-        System.out.println("This is the program that calculate points. We've got points for the style, effects and result-each one is a different weight point");
+        System.out.println("This is a program designed to calculate the final score for a three groups of points with different weights.");
+        System.out.println("We have got points for a style, effects and the result");
+
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Style: ");
-        double styl = scanner.nextDouble() * 0.2;
-        
-        System.out.println("Effects: ");
+
+        System.out.println("Please enter the points for a style: ");
+        double style = scanner.nextDouble() * 0.2;
+
+        System.out.println("Please enter the points for the effects: ");
         double effects = scanner.nextDouble() * 0.3;
-        
-        System.out.println("Result: ");
-        double result = scanner.nextDouble() * 0.5;
-        
-        double suma = styl + effects + result;
-        System.out.println("_____________________");
-        System.out.println("Ok, the sum of the points is: ");
-        System.out.println(suma);
+
+        System.out.println("Please enter the points for a style: ");
+        double rezult = scanner.nextDouble() * 0.5;
+
+        double sum;
+        sum = style + effects + rezult;
+        sum *= 100; //rounding the result
+        sum = Math.round(sum);
+        sum /= 100;
+
+
+
+        System.out.println("The final score is: ");
+        System.out.println(sum);
+
+
+
     }
 }
